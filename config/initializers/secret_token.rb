dae27e0207137ef3aabb02cd8da9b241db767990
Shuai Yuan <shuaisyn@gmail.com>
2013-08-18 12:50:06 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+HighandZ::Application.config.secret_key_base = '1ad54bd87747b02bb61b6d1d34312c914ad7a37c012442af230bcd2f59dedb7a695ceb6943cea5707093d2a8d23406f2d7ca3541ffb74d3e788987f6a10783e4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+HighandZ::Application.config.session_store :cookie_store, key: '_highand_z_session'
