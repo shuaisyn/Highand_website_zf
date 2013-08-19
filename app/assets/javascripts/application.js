21c95ec3b1f29d40ce5d2686b44eb48a3ef1e077
Shuai Yuan <shuaisyn@gmail.com>
2013-08-20 00:25:32 +0800

    for detals

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,33 @@
 //= require_tree .
 
 $(function(){ $(document).foundation(); });
+
+$(document).foundation('orbit', {
+  animation: 'fade',
+  timer_speed: 4000,
+  pause_on_hover: true,
+  resume_on_mouseout: false,
+  animation_speed: 500,
+  stack_on_small: false,
+  navigation_arrows: true,
+  slide_number: true,
+  container_class: 'orbit-container',
+  stack_on_small_class: 'orbit-stack-on-small',
+  next_class: 'orbit-next',
+  prev_class: 'orbit-prev',
+  timer_container_class: 'orbit-timer',
+  timer_paused_class: 'paused',
+  timer_progress_class: 'orbit-progress',
+  slides_container_class: 'orbit-slides-container',
+  bullets_container_class: 'orbit-bullets',
+  bullets_active_class: 'active',
+  slide_number_class: 'orbit-slide-number',
+  caption_class: 'orbit-caption',
+  active_slide_class: 'active',
+  orbit_transition_class: 'orbit-transitioning',
+  bullets: true,
+  timer: true,
+  variable_height: false,
+  before_slide_change: function(){},
+  after_slide_change: function(){}
+});
