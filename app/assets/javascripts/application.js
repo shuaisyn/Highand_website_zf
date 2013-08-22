fa4493193889ba4f067e2f3566d118a92265ca49
Shuai Yuan <shuaisyn@gmail.com>
2013-08-22 11:30:24 +0800

    adjust animation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,12 +19,12 @@
 $(function(){ $(document).foundation(); });
 
 $(document).foundation('orbit', {
-  animation: 'fade',
+  animation: 'slide',
   timer_speed: 4000,
   pause_on_hover: true,
   resume_on_mouseout: false,
   animation_speed: 500,
-  stack_on_small: true,
+  stack_on_small: false,
   navigation_arrows: true,
   slide_number: true,
   container_class: 'orbit-container',
@@ -42,7 +42,7 @@ $(document).foundation('orbit', {
   active_slide_class: 'active',
   orbit_transition_class: 'orbit-transitioning',
   bullets: true,
-  timer: true,
+  timer: false,
   variable_height: false,
   before_slide_change: function(){},
   after_slide_change: function(){}
